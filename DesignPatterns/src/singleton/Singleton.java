39173420752dce0d325b92f8d508acad8bd37826
Maicon <maicon.fermo@gmail.com>
2013-07-29 21:47:37 -0300

    Primeiro Commit

---
+++ DesignPatterns/src/singleton/Singleton.java
@@ -0,0 +1,15 @@
+package singleton;
+
+public class Singleton {
+
+	private String dado;
+	
+	public String getDado() {
+		return dado;
+	}
+	
+	public void setDado(String dado) {
+		this.dado = dado;
+	}
+	
+}
